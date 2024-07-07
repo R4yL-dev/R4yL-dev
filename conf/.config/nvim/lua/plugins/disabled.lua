ccf6ae9d7a733d6e341bcef953558b04a34ffb24
R4yL <luca.ray@protonmail.ch>
2024-07-08 01:12:11 +0200

    enable noice

--- conf/.config/nvim/lua/plugins/disabled.lua
+++ conf/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "folke/noice", enabled = false },
 	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
 	{ "akinsho/bufferline.nvim", enabled = false },
 	{ "nvimdev/dashboard-nvim", enabled = false },
