3d947f54348499e138e23265ec6620916fb21057
R4yL <luca.ray@protonmail.ch>
2025-12-25 15:39:54 +0100

    delete usless stuffs

--- conf/.config/nvim/lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
-	{ "akinsho/bufferline.nvim", enabled = false },
-	{ "nvimdev/dashboard-nvim", enabled = false },
-	{ "rcarriga/nvim-notify", enabled = false },
-}
